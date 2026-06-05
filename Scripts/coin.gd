22e384b43bb3802eb8d63e13b771caab6740e08a
Pixtol the Maker <aldocarmona308@gmail.com>
2026-06-04 19:33:22 -0700

    class 6-4
    added new enemy and coin counter, along with coin sound and music
    
    need to add new item later and that is it, all is done :)

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,11 +1,19 @@
 extends Area2D
-
+@onready var game_master: GameManager = %"Game Master"
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 
 func _on_body_entered(body: Node2D) -> void:
-	var player :PlayerBody2D
-	player = body as PlayerBody2D
-	player.coins += 1
-	print(player.coins)
-	queue_free()
+	audio_stream_player_2d.play()
+	game_master.add_points(1)
+	collision_shape_2d.set_deferred("disabled", true)
+	animated_sprite_2d.visible = false
+	timer.start()
 	
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
---
+++ Scripts/fast_slime.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var speed : float = 50
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var enemy_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var facing_left : bool = true
+
+func _physics_process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		facing_left = false
+		enemy_sprite.flip_h = true
+	if ray_cast_right.is_colliding():
+		facing_left = true
+		enemy_sprite.flip_h = false
+	
+	
+	if  facing_left:
+		position.x += -speed * delta
+	else:
+		position.x += speed * delta
